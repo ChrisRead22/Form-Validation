b2a02c8cfdb1d8626202433516516f717fec01da
Christopher Read <Christopherread22@outlook.com>
2021-07-22 18:04:52 -0400

    FormSuccess

--- src/FormSuccess.js
+++ src/FormSuccess.js
@@ -5,7 +5,7 @@ const FormSuccess = () => {
         <div className="form-content-right">
             <div className="form-success">We have received
             your request!</div>
-            
+            <img src="img/img-3.svg" alt="success-image" className="form-img-2"/>
         </div>
     )
 }
