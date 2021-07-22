3e86302cf99244d03e05ed9607f3c3c037a7ccb6
Christopher Read <Christopherread22@outlook.com>
2021-07-22 17:47:47 -0400

    FormSuccess

--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
 
 function Form() {
     const[isSubmitted, setIsSubmitted] = useState(false)
@@ -11,6 +12,8 @@ function Form() {
     return (
         <div>
         <FormSignup />
+        {!isSubmitted ? <FormSignup submitForm=
+        {submitForm} /> : <FormSuccess />}
         </div>
     );
 };
