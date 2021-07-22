f16580efb2bea97c4cc887147f24aee1acf28219
Christopher Read <Christopherread22@outlook.com>
2021-07-21 20:01:31 -0400

    validation

--- src/validateInfo.js
+++ src/validateInfo.js
@@ -2,6 +2,10 @@ export default function validateInfo(values) {
     let errors = {}
 
     if(!values.username.trim()) {
-        
+        errors.username = "Username required"
+    }
+
+    if(!values.email) {
+        errors.email = "Email required"
     }
 }
