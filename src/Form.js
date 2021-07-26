d20ac0c0919e8e516b80371fa894a3de09e5f99b
Christopher Read <Christopherread22@outlook.com>
2021-07-26 19:03:52 -0400

    Refactoring

--- src/Form.js
+++ src/Form.js
@@ -14,13 +14,14 @@ function Form() {
         <div className="form-container">
             <span className="close-btn">x</span>
             <div className="form-content-left">
-                <img src="img/img-2." alt="" className="form-img"/>
+                <img src="img/img-2." alt="spaceship" 
+                className="form-img"/>
             </div>
+            {!isSubmitted ? <FormSignup submitForm=
+        {submitForm} /> : <FormSuccess />}
         </div>
         
-        {!isSubmitted ? <FormSignup submitForm=
-        {submitForm} /> : <FormSuccess />}
-       
+        
     );
 };
 
